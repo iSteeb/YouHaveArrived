39bc7c02aafe4e3e26c88dba351d58e2b529ca0a
Steven Duzevich <steven@duz.ie>
2023-11-15 17:04:55 +0100

    Initial Commit

---
+++ YouHaveArrived Watch App/YouHaveArrivedApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouHaveArrivedApp.swift
+//  YouHaveArrived Watch App
+//
+//  Created by Steven Duzevich on 15/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct YouHaveArrived_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
